985e4785f60c6405cdc2874fdf8b2b3fd16a1105
Ben McGinnes <ben@adversary.org>
2014-09-20 13:38:03 +1000

    Specified multiple licensing options, used a better method of extracting the version number (left it a string for now) and set os.walk to check directories from the bottom first to reduce loading time.

--- vlc/bindings/python/agnostic/vlc/__init__.py
+++ vlc/bindings/python/agnostic/vlc/__init__.py
@@ -1,8 +1,12 @@
 # A Python package, what a surprise!
 
-# VLC Version checker.
+# VLC and Python Version checker.
 #
 # Author: Ben McGinnes <ben@adversary.org>
+#
+# Provided to the VideoLAN Project under the terms of the LGPL 2.1 or
+# any later version, may optionally be licensed under the terms of the
+# GPL 3.0 or later (dual licensed).
 
 import os
 import os.path
@@ -10,9 +14,8 @@ import subprocess
 import sys
 import inspect
 
-# Just in case people are using Python 3:
+# Just in case people are using Python 3, sets VLC module path:
 
-# Sets VLC module path:
 cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
 if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
@@ -32,13 +35,13 @@ else:
     topdir = os.path.abspath("/")
 
 def vlcpath(vlcx, topdir):
-    for root, dirs, files in os.walk(topdir):
+    for root, dirs, files in os.walk(topdir, topdown=False):
         if vlcx in files:
             return os.path.join(root, vlcx)
 
 vlcexec = vlcpath(vlcx, topdir)
 vlc_ver = subprocess.Popen([vlcexec, "--version"], stdout=subprocess.PIPE).communicate()[0]
-vlcver = vlc_ver[12:20]  # in case version numbers get huge, otherwise [12:15]
+vlcver = vlc_ver.split(" ")[2]
 
 if vlcver.startswith(b"1.0"):
     from vlc_100 import *
